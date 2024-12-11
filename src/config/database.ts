75001a4a30bc2a4244a4fa8c3c598db913182358
Lucas Santana <35945897-lucassantanam@users.noreply.replit.com>
2024-12-11 22:30:33 +0000

    Cria a estrutura de pastas do projeto e configura a conexão com o banco de dados

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const connectionString =
+  "postgresql://postgres:sfWkwbAxnebCpQoLnBWVCEnWqVxcRqap@autorack.proxy.rlwy.net:28278/railway";
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
