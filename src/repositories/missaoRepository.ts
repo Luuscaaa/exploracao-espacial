2d4e8d15fec92b77543317f87240a0b43cbb68a0
Lucas Santana <35945897-lucassantanam@users.noreply.replit.com>
2024-12-11 23:31:33 +0000

    Adiciona um tratamento de erros e a verificação dos parametros

--- src/repositories/missaoRepository.ts
+++ src/repositories/missaoRepository.ts
@@ -1,5 +1,5 @@
-import pool from '../config/database';
-import { Missao } from '../models/missaoModel';
+import pool from "../config/database";
+import { Missao } from "../models/missaoModel";
 
 export class MissaoRepository {
   // Método para criar uma nova missão
@@ -9,20 +9,42 @@ export class MissaoRepository {
       VALUES ($1, $2, $3, $4, $5, $6)
       RETURNING *;
     `;
-    const result = await pool.query(query, [missao.nome, missao.planeta_alvo, missao.duracao_anos, missao.tripulantes, missao.tecnologias, missao.image || null]);
-    return result.rows[0];  // Retorna a missão criada
+    try {
+      const result = await pool.query(query, [
+        missao.nome,
+        missao.planeta_alvo,
+        missao.duracao_anos,
+        missao.tripulantes,
+        missao.tecnologias,
+        missao.image || null,
+      ]);
+      return result.rows[0]; // Retorna a missão criada
+    } catch (err) {
+      console.error("Erro ao criar missão no banco de dados:", err); // Log de erro
+      throw new Error("Erro ao criar missão"); // Lançando erro para o serviço
+    }
   }
 
   // Método para listar todas as missões
   async findAll(): Promise<Missao[]> {
-    const query = 'SELECT * FROM missoes;';
-    const result = await pool.query(query);
-    return result.rows;
+    const query = "SELECT * FROM missoes;";
+    try {
+      const result = await pool.query(query);
+      return result.rows;
+    } catch (err) {
+      console.error("Erro ao listar missões no banco de dados:", err); // Log de erro
+      throw new Error("Erro ao listar missões");
+    }
   }
 
   // Método para excluir uma missão pelo ID
   async delete(id: number): Promise<void> {
-    const query = 'DELETE FROM missoes WHERE id = $1;';
-    await pool.query(query, [id]);
+    const query = "DELETE FROM missoes WHERE id = $1;";
+    try {
+      await pool.query(query, [id]);
+    } catch (err) {
+      console.error("Erro ao excluir missão no banco de dados:", err); // Log de erro
+      throw new Error("Erro ao excluir missão");
+    }
   }
 }
